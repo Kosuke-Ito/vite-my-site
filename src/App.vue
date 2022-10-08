c81cf2947b3f690ae5669e51a95a6c1481a01861
Kosuke Ito <keree.it.owl.uk@gmail.com>
2022-10-09 01:25:06 +0900

    TopPageの作成

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import TopPage from "./components/TopPage.vue";
+</script>
+
+<template>
+  <TopPage msg="Kosuke Ito Portfolio" />
+</template>
+
+<style scoped>
+
+</style>
