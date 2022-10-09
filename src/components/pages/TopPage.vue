16ea1d9e4dbe8e8f5da78652a0799cdf09a70ec8
Kosuke Ito <keree.it.owl.uk@gmail.com>
2022-10-09 23:04:51 +0900

    階層変更

---
+++ src/components/pages/TopPage.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+
+const items = ref(["about", "works", "blogs", "contact"])
+defineProps({
+  msg: String
+})
+
+</script>
+
+<template>
+  <header>
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li v-for="(item, key) in items" :key="key">
+        {{ item }}
+      </li>
+    </ul>
+  </header>
+</template>
+
+<style scoped>
+header{display: flex;align-items: center}
+h1{margin-right: auto;}
+ul {list-style: none; display: flex;}
+li{margin-right: 1rem;}
+
+
+
+</style>
