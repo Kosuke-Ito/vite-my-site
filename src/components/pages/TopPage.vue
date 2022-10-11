2ea891799859aa2d74590e54b581e51b835928ff
Kosuke Ito <keree.it.owl.uk@gmail.com>
2022-10-11 23:28:02 +0900

    コンポーネントの修正

--- src/components/pages/TopPage.vue
+++ src/components/pages/TopPage.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { ref } from 'vue'
+import Engineering from "../containers/Engineering.vue"
+import Photograph from "../containers/Photograph.vue"
 
-const items = ref(["about", "works", "blogs", "contact"])
+const items = ref(["works", "about", "blogs", "contact"])
 defineProps({
   msg: String
 })
@@ -9,18 +11,23 @@ defineProps({
 </script>
 
 <template>
-  <header>
-    <h1>{{ msg }}</h1>
-    <ul>
-      <li v-for="(item, key) in items" :key="key">
-        {{ item }}
-      </li>
-    </ul>
-  </header>
+<header>
+  <h1>{{ msg }}</h1>
+  <ul>
+    <li v-for="(item, key) in items" :key="key">
+      {{ item }}
+    </li>
+  </ul>
+</header>
+<div id="content">
+  <Engineering />
+  <Photograph />
+</div>
 </template>
 
 <style scoped>
-header{display: flex;align-items: center}
+header{display: flex;align-items: center;margin: 0 1rem;}
+#content{background-color: blue;}
 h1{margin-right: auto;}
 ul {list-style: none; display: flex;}
 li{margin-right: 1rem;}
