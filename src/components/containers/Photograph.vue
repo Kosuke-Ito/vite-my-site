a2daf944e89ef46a29c0b4b31f130fc7cc8b63b8
Kosuke Ito <keree.it.owl.uk@gmail.com>
2022-10-11 23:27:47 +0900

    Photographコンポーネントの追加

---
+++ src/components/containers/Photograph.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+
+</script>
+
+<template>
+<div id="photograph">
+  <h2>Photograph</h2>
+</div>
+</template>
+
+<style scoped>
+#photograph{background-color: rgb(55, 65, 72); }
+h2{font-size:3rem;margin: 0;}
+</style>
