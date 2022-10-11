23afc72e1610177f7c624faa4aaa288bf0b0191d
Kosuke Ito <keree.it.owl.uk@gmail.com>
2022-10-11 23:27:32 +0900

    Engineerringコンポーネントの追加

---
+++ src/components/containers/Engineering.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+
+</script>
+
+<template>
+<div id="Engineering">
+  <h2>Engineering</h2>
+</div>
+</template>
+
+<style scoped>
+#Engineering{background-color:darkblue;}
+h2{font-size:3rem;margin:0;}
+</style>
