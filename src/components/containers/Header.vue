a84f9116dbbb81b172dbcdc00851a6f2224d9cf4
Kosuke Ito <keree.it.owl.uk@gmail.com>
2022-10-21 01:54:09 +0900

    headerコンポーネンんを追加

---
+++ src/components/containers/Header.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+defineProps({
+  msg: String
+})
+const items = ref(["works", "about", "blogs", "contact"])
+</script>
+
+<template>
+<div class="navbar flex">
+  <h1 class="text-3xl font-bold w-1/2">
+    <a>{{ msg }}</a>
+  </h1>
+  <ul class="menu menu-horizontal w-1/2">
+    <li v-for="(item, key) in items" :key="key">
+      <a>{{ item }}</a>
+    </li>
+  </ul>
+</div>
+</template>
+
+<style scoped>
+.navbar{align-items: center;margin: 1rem;}
+</style>
+
--- src/components/pages/TopPage.vue
+++ src/components/pages/TopPage.vue
@@ -1,43 +1,21 @@
 <script setup>
 import { ref } from 'vue'
+import Header from "../containers/Header.vue"
 import Engineering from "../containers/Engineering.vue"
 import Photograph from "../containers/Photograph.vue"
-
-const items = ref(["works", "about", "blogs", "contact"])
-defineProps({
-  msg: String
-})
-
 </script>
 
 <template>
-  <header class="navbar bg-base-100 bg-primary accent">
-    <div class="flex-1 btn">
-      <h1 class="text-3xl font-bold">
-        <a>{{ msg }}</a>
-      </h1>
-    </div>
-    <div class="flex-none">
-      <ul class="menu menu-horizontal p-0">
-        <li v-for="(item, key) in items" :key="key">
-          <a>{{ item }}</a>
-        </li>
-      </ul>
-    </div>
-  </header>
-  <div id="content">
+  <Header msg="Kosuke Ito Portfolio"/>
+  <div id="content flex">
     <Engineering />
     <Photograph />
   </div>
 </template>
 
 <style scoped>
-header{align-items: center;margin: 1rem;}
 #content{background-color: blue; padding: 0 1rem;}
 h1{margin-right: auto;}
 ul {list-style: none;}
 li{margin-right: 1rem;}
-
-
-
 </style>
