ccc615d81e451fc2d9eddbd54b78cda9e99faae4
Kosuke Ito <keree.it.owl.uk@gmail.com>
2022-11-11 00:25:43 +0900

    nacbarコンポーネント追加

---
+++ src/components/parts/Navbar.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+const items = ref(["about", "blog", "contact"])
+</script>
+
+<template>
+  <div class="navbar">
+    <a class="text-xl font-bold" href="#">
+      <h1>{{ msg }}</h1>
+    </a>
+    <ul class="menu">
+      <li v-for="item in items" :key="item.key" class="text-lg font-bold">
+        <a :href="`/${item}`">{{item}}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.navbar{padding: 1rem;display: flex;}
+.menu{display: flex;}
+.menu li{flex: 1 0 auto;}
+</style>
