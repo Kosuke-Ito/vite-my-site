0788c2a2c83630aa4c9021030b27ceef6c126bd6
Kosuke Ito <keree.it.owl.uk@gmail.com>
2022-11-17 00:49:51 +0900

    photographページの作成

---
+++ src/components/parts/Photograph.vue
@@ -0,0 +1,26 @@
+<script>
+import { ref } from 'vue'
+
+</script>
+
+
+<template>
+<div id="photograph">
+  <h2>Photograph</h2>
+</div>
+</template>
+
+<style scoped>
+#photograph{
+  margin:1rem;
+  height: 20rem;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: pink;
+}
+h2{font-size: 5rem;margin: 0;}
+
+</style>
