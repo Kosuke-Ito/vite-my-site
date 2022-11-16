07966903f1a7e5d881aaf021b5a893650ca88ce1
Kosuke Ito <keree.it.owl.uk@gmail.com>
2022-11-17 00:49:18 +0900

    Engineeringページの作成

---
+++ src/components/parts/Engineering.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<template>
+<div id="engineering">
+  <h2>Engineering</h2>
+</div>
+</template>
+
+
+<style scoped>
+#engineering{
+  margin:1rem;
+  height: 20rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: cadetblue;
+}
+h2{font-size: 5rem;margin: 0;}
+
+</style>
