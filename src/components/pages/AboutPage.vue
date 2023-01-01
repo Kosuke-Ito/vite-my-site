e042663d32e5137f3acf325fdf0271eb2346cd71
Kosuke Ito <keree.it.owl.uk@gmail.com>
2023-01-01 22:24:22 +0900

    Aboutページ作成

---
+++ src/components/pages/AboutPage.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+import Navbar from '../parts/Navbar.vue';
+
+</script>
+
+<template>
+<Navbar msg="Kosuke Ito"/>
+
+<h2>Ito Kosuke</h2>
+</template>
+
+<style scoped>
+
+</style>
